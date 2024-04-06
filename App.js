0027007c2365a88c8f4979061660b4b0d011a9ca
ashujarodia <112541439+ashujarodia@users.noreply.github.com>
2024-04-06 14:23:15 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,89 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import { useState } from 'react';
+import { ImageBackground, SafeAreaView, StyleSheet } from 'react-native';
+
+import Colors from './constants/colors';
+import GameOverScreen from './screens/GameOverScreen';
+import GameScreen from './screens/GameScreen';
+import StartGameScreen from './screens/StartGameScreen';
+
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+	const [userNumber, setUserNumber] = useState();
+	const [gameIsOver, setGameIsOver] = useState(true);
+	const [guessRounds, setGuessRounds] = useState(0);
+
+	// useFonts({
+	// 	'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+	// 	'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+	// });
+
+	// if (!fontsLoaded) {
+	// 	return <AppLoading />;
+	// }
+
+	function pickedNumberHandler(pickedNumber) {
+		setUserNumber(pickedNumber);
+		setGameIsOver(false);
+	}
+
+	function gameOverHandler(numberOfRounds) {
+		setGameIsOver(true);
+		setGuessRounds(numberOfRounds);
+	}
+
+	function startNewGameHandler() {
+		setUserNumber(null);
+		setGuessRounds(0);
+	}
+
+	let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
+	if (userNumber) {
+		screen = (
+			<GameScreen
+				userNumber={userNumber}
+				onGameOver={gameOverHandler}
+			/>
+		);
+	}
+
+	if (gameIsOver && userNumber) {
+		screen = (
+			<GameOverScreen
+				userNumber={userNumber}
+				roundsNumber={guessRounds}
+				onStartNewGame={startNewGameHandler}
+			/>
+		);
+	}
+
+	return (
+		<>
+			<StatusBar style='light' />
+			<LinearGradient
+				colors={[Colors.primary700, Colors.accent500]}
+				style={styles.rootScreen}
+			>
+				<ImageBackground
+					source={require('./assets/images/background.png')}
+					resizeMode='cover'
+					style={styles.rootScreen}
+					imageStyle={styles.backgroundImage}
+				>
+					<SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+				</ImageBackground>
+			</LinearGradient>
+		</>
+	);
+}
+
+const styles = StyleSheet.create({
+	rootScreen: {
+		flex: 1,
+	},
+	backgroundImage: {
+		opacity: 0.15,
+	},
+});
